c2560c483797aa0195f25339149f3d25dfd2e6a4
MightyCode <bazinmaxence@free.fr>
2024-08-17 08:33:46 +0200

    add j'ai oublié de push les scènes

--- GMTKJam2024UnityProject/Assets/Scripts/Systems/GameManager.cs
+++ GMTKJam2024UnityProject/Assets/Scripts/Systems/GameManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public TextMeshPro text;
+
+    private Timer timer;
+    public float HarvestTime = 10;
+
+    public bool Harvesting = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = new Timer();
+        timer.Start(HarvestTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Harvesting)
+        {
+            timer.Update();
+
+            float remainingTime = HarvestTime - timer.ElapsedTime;
+
+            if (remainingTime <= 0)
+            {
+                Harvesting = false;
+                text.text = "0.0.0";
+            }
+            else
+            {
+                text.text = ;
+            }
+
+        }
     }
 }
