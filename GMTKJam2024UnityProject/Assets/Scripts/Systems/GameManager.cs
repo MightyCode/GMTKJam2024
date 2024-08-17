a9541339718eb1bcbdeb0ba93f50b8b61b6c1f62
MightyCode <bazinmaxence@free.fr>
2024-08-17 13:39:11 +0200

    the beast class controll certain things

--- GMTKJam2024UnityProject/Assets/Scripts/Systems/GameManager.cs
+++ GMTKJam2024UnityProject/Assets/Scripts/Systems/GameManager.cs
@@ -8,17 +8,7 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private TMP_Text timerText;
-
-    [SerializeField]
     private TMP_Text resourceCounterText;
-
-    private Timer timer;
-
-    [SerializeField]
-    float HarvestTime = 10;
-    [SerializeField]
-    bool Harvesting = true;
 
     [SerializeField]
     private GameObject player;
@@ -26,51 +16,14 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = new Timer();
-        timer.Start(HarvestTime);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Harvesting)
-        {
-            timer.Update();
 
-            float remainingTime = HarvestTime - timer.ElapsedTime;
-            TimeSpan time = TimeSpan.FromSeconds(remainingTime);
-
-            if (remainingTime <= 0)
-            {
-                Harvesting = false;
-                timerText.text = "00:00:00:000";
-                timerText.color = Color.red;
-            }
-            else
-            {
-                timerText.text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}", 
-                    time.Hours, time.Minutes, time.Seconds, time.Milliseconds);
-            }
-
-                // color from blue to red
-                timerText.color = Color.Lerp(Color.red, Color.green, remainingTime / HarvestTime);
-        }
 
         resourceCounterText.text = player.GetComponent<PlayerManager>().Resource.ToString();
-    }
-
-    public void BlockHarverst()
-    {
-        Harvesting = false;
-    }
-    public void UnblockHarverst()
-    {
-        Harvesting = true;
-    }
-
-    public void ResetHarverst()
-    {
-        timer.ResetStart();
-        Harvesting = true;
     }
 }
---
+++ GMTKJam2024UnityProject/Assets/Scripts/Systems/TheBeast.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class TheBeast : MonoBehaviour
+{
+    [SerializeField] private GameManager manager;
+    [SerializeField] private PlayerManager player;
+
+    [SerializeField]
+    private TMP_Text timerText;
+
+    private Timer feedTimer;
+
+    [SerializeField]
+    float waitingForFeeding = 20;
+    [SerializeField]
+    bool IsWaiting = true;
+
+    [SerializeField]
+    private TMP_Text foodText;
+    public float FoodGoal;
+    private float currentFood;
+
+    [FormerlySerializedAs("collectSuccesfull")]
+    [SerializeField]
+    private UnityEvent m_onEnter = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        feedTimer = new Timer();
+        feedTimer.Start(waitingForFeeding);
+
+        foodText.text = currentFood + "/" + FoodGoal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsWaiting)
+        {
+            feedTimer.Update();
+
+            float remainingTime = waitingForFeeding - feedTimer.ElapsedTime;
+            TimeSpan time = TimeSpan.FromSeconds(remainingTime);
+
+            if (remainingTime <= 0)
+            {
+                IsWaiting = false;
+                timerText.text = "00:00:00:000";
+                timerText.color = Color.red;
+            }
+            else
+            {
+                timerText.text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
+                    time.Hours, time.Minutes, time.Seconds, time.Milliseconds);
+            }
+
+            // color from blue to red
+            timerText.color = Color.Lerp(Color.red, Color.green, remainingTime / waitingForFeeding);
+        }
+    }
+
+    public void BlockHarverst()
+    {
+        IsWaiting = false;
+    }
+    public void UnblockHarverst()
+    {
+        IsWaiting = true;
+    }
+
+    public void ResetHarverst()
+    {
+        feedTimer.ResetStart();
+        IsWaiting = true;
+    }
+
+    public void Collect()
+    {
+        if (feedTimer.Finished)
+        {
+            // TODO MORT
+        } else
+        {
+            currentFood += player.Resource;
+            player.RemoveResource(player.Resource);
+            Debug.Log("J'ia bien mangé" + currentFood);
+
+            if (currentFood >= FoodGoal)
+            {
+               // Réussite jeu
+            } else
+            {
+
+                foodText.text = currentFood + "/" + FoodGoal;
+                
+                m_onEnter.Invoke();
+            }
+        }
+    }
+}
